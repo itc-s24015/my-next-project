dab45d682d7b37e67f23d2e8e7d0da6fe7cc820b
itc-s24015 <s24015@std-it.college.ac.jp>
2024-12-19 07:03:59 +0000

    最後の方までできたけど画面が大きい

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import Footer from "./_components/Footer";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ja">
       <body>
