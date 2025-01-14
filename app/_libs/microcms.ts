8730ab3a0307db5ac3603092cfaef7871eadd519
itc-s24015 <s24015@std-it.college.ac.jp>
2025-01-14 07:56:19 +0000

    138poまで完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,3 +1,19 @@
+import { createClient } from "microcms-js-sdk";
+import type{
+  MicroCMSQueries,
+  MicroCMDImage,
+  MicroCMSListContet,
+} from "microcms-js-sdk";
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMDImage;
+} & MicroCMSListContet;
+
+export type category ={
+  name: string;
+}
 export type News = {
   id: string;
   title: string;
