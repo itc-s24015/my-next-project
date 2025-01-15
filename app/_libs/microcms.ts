9d02286b313d152c5a1207e7d17f779cf9bea02c
itc-s24015 <s24015@std-it.college.ac.jp>
2025-01-15 07:36:33 +0000

    5章まで完了

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,25 +1,54 @@
 import { createClient } from "microcms-js-sdk";
-import type{
+import type {
   MicroCMSQueries,
-  MicroCMDImage,
-  MicroCMSListContet,
+  MicroCMSImage,
+  MicroCMSListContent,
 } from "microcms-js-sdk";
+
 export type Member = {
   name: string;
   position: string;
   profile: string;
-  image: MicroCMDImage;
-} & MicroCMSListContet;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
 
-export type category ={
+export type Category = {
   name: string;
-}
+} & MicroCMSListContent;
+
 export type News = {
-  id: string;
   title: string;
-  category: {
-    name: string;
-  };
-  publishedAt: string;
-  createdeat: string;
+  description: string;
+  content: string;
+  thumbnail?: MicroCMSImage;
+  category: Category;
+} & MicroCMSListContent;
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData;
+};
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<News>({
+    endpoint: "news",
+    queries,
+  });
+  return listData;
 };
--- app/_libs/utils.ts
+++ app/_libs/utils.ts
@@ -1,10 +1,10 @@
-import dayjs from "adayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
+// import dayjs from "adayjs";
+// import utc from "dayjs/plugin/utc";
+// import timezone from "dayjs/plugin/timezone";
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
+// dayjs.extend(utc);
+// dayjs.extend(timezone);
 
-export const formatDate = (date: string) => {
-  return dayjs.utc(date).tz("Asia/Tokyo").format("YYYY/MM/DD");
-};
+// export const formatDate = (date: string) => {
+//   return dayjs.utc(date).tz("Asia/Tokyo").format("YYYY/MM/DD");
+// };
