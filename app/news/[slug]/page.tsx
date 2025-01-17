0f287da3cf97fa2d28cda895cade494121a62bad
itc-s24015 <s24015@std-it.college.ac.jp>
2025-01-17 01:54:14 +0000

    ６章まで完了

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,46 +1,21 @@
-import { notFound } from "next/navigation";
 import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
-import styles from "./page.module.css";
+import styles from "./page.module.css"
 
 type Props = {
-  params: {
-    slug: string;
-  };
+    params: {
+        slug: string
+    };
 };
 
-export default async function Page({ params }: Props) {
-  const data = await getNewsDetail(params.slug).catch(notFound);
+export default async function Page({params}: Props) {
+  const data = await getNewsDetail(params.slug);
 
   return (
     <>
       <Article data={data} />
-      <div className={styles.footer}>
-        <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
-      </div>
-    </>
-  );
-}
-
-import { notFound } from "next/navigation";
-import { getNewsDetail } from "@/app/_libs/microcms";
-import Article from "@/app/_components/Article";
-import ButtonLink from "@/app/_components/ButtonLink";
-import styles from "./page.module.css";
-
-type Props = {
-  params: {
-    slug: string;
-  };
-};
-
-export default async function Page({ params }: Props) {
-  const data = await getNewsDetail(params.slug).catch(notFound);
-
-  return (
-    <>
-      <Article data={data} />
+      any
       <div className={styles.footer}>
         <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
       </div>
